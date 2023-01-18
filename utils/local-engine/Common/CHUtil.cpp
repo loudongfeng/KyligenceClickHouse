liuneng1994/ClickHouse

support log profile event after query

utils/local-engine/Common/CHUtil.cpp
@@ -8,7 +8,9 @@
 #include <Processors/Chunk.h>
 #include <Processors/QueryPlan/QueryPlan.h>
 #include <Core/ColumnsWithTypeAndName.h>
+#include <fmt/core.h>
 #include <DataTypes/DataTypesNumber.h>
+#include <base/logger_useful.h>
 
 namespace DB
 {
@@ -110,4 +112,19 @@ DB::NamesAndTypesList MergeTreeUtil::getSchemaFromMergeTreePart(const fs::path &
     return names_types_list;
 }
 
+void ProfileEventsUtil::logProfileEvents(std::map<String, size_t> events)
+{
+    auto * logger = &Poco::Logger::get("ProfileEventsUtil");
+    if (events.empty() || !logger->debug())
+    {
+        return;
+    }
+
+    std::vector<String> event_strings;
+    for (auto& [name, count] : events)
+    {
+        event_strings.emplace_back(fmt::format("\t{} : {}", name, count));
+    }
+    LOG_DEBUG(logger, "Current Profile Counters:\n{}\n", fmt::join(event_strings, "\n"));
+}
 }

utils/local-engine/Parser/SerializedPlanParser.cpp
@@ -1,6 +1,4 @@
 #include <memory>
-#include <base/logger_useful.h>
-#include <base/Decimal.h>
 #include <AggregateFunctions/AggregateFunctionFactory.h>
 #include <AggregateFunctions/registerAggregateFunctions.h>
 #include <Builder/BroadCastJoinBuilder.h>
@@ -9,16 +7,15 @@
 #include <Core/Names.h>
 #include <Core/NamesAndTypes.h>
 #include <DataTypes/DataTypeAggregateFunction.h>
+#include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeDate32.h>
 #include <DataTypes/DataTypeDateTime64.h>
+#include <DataTypes/DataTypeMap.h>
 #include <DataTypes/DataTypeSet.h>
 #include <DataTypes/DataTypeString.h>
+#include <DataTypes/DataTypeTuple.h>
 #include <DataTypes/DataTypesDecimal.h>
 #include <DataTypes/DataTypesNumber.h>
-#include <DataTypes/DataTypeDateTime64.h>
-#include <DataTypes/DataTypeArray.h>
-#include <DataTypes/DataTypeMap.h>
-#include <DataTypes/DataTypeTuple.h>
 #include <Functions/FunctionFactory.h>
 #include <Functions/registerFunctions.h>
 #include <Interpreters/ActionsDAG.h>
@@ -42,11 +39,9 @@
 #include <Storages/MergeTree/MergeTreeData.h>
 #include <Storages/StorageMergeTreeFactory.h>
 #include <Storages/SubstraitSource/SubstraitFileSource.h>
-#include <base/logger_useful.h>
 #include <google/protobuf/util/json_util.h>
 #include <Poco/StringTokenizer.h>
 #include <Poco/Util/MapConfiguration.h>
-#include <Common/DebugUtils.h>
 #include <Common/JoinHelper.h>
 #include <Common/MergeTreeTool.h>
 #include <Common/StringUtils.h>
@@ -185,9 +180,9 @@ QueryPlanPtr SerializedPlanParser::parseReadRealWithJavaIter(const substrait::Re
 
 void SerializedPlanParser::addRemoveNullableStep(QueryPlan & plan, std::vector<String> columns)
 {
-    if (columns.empty()) return;
-    auto remove_nullable_actions_dag
-        = std::make_shared<ActionsDAG>(blockToNameAndTypeList(plan.getCurrentDataStream().header));
+    if (columns.empty())
+        return;
+    auto remove_nullable_actions_dag = std::make_shared<ActionsDAG>(blockToNameAndTypeList(plan.getCurrentDataStream().header));
     removeNullable(columns, remove_nullable_actions_dag);
     auto expression_step = std::make_unique<ExpressionStep>(plan.getCurrentDataStream(), remove_nullable_actions_dag);
     expression_step->setStepDescription("Remove nullable properties");
@@ -207,7 +202,7 @@ QueryPlanPtr SerializedPlanParser::parseMergeTreeTable(const substrait::ReadRel 
     else
     {
         // For count(*) case, there will be an empty base_schema, so we try to read at least once column
-        auto all_parts_dir = MergeTreeUtil::getAllMergeTreeParts( std::filesystem::path("/") / merge_tree_table.relative_path);
+        auto all_parts_dir = MergeTreeUtil::getAllMergeTreeParts(std::filesystem::path("/") / merge_tree_table.relative_path);
         if (all_parts_dir.empty())
         {
             throw DB::Exception(DB::ErrorCodes::LOGICAL_ERROR, "Empty mergetree directory: {}", merge_tree_table.relative_path);
@@ -266,13 +261,14 @@ QueryPlanPtr SerializedPlanParser::parseMergeTreeTable(const substrait::ReadRel 
     if (!not_null_columns.empty())
     {
         auto input_header = query->getCurrentDataStream().header;
-        std::erase_if(not_null_columns, [input_header](auto item) -> bool {return !input_header.has(item);});
+        std::erase_if(not_null_columns, [input_header](auto item) -> bool { return !input_header.has(item); });
         addRemoveNullableStep(*query, not_null_columns);
     }
     return query;
 }
 
-PrewhereInfoPtr SerializedPlanParser::parsePreWhereInfo(const substrait::Expression & rel, Block & input, std::vector<String>& not_nullable_columns)
+PrewhereInfoPtr
+SerializedPlanParser::parsePreWhereInfo(const substrait::Expression & rel, Block & input, std::vector<String> & not_nullable_columns)
 {
     auto prewhere_info = std::make_shared<PrewhereInfo>();
     prewhere_info->prewhere_actions = std::make_shared<ActionsDAG>(input.getNamesAndTypesList());
@@ -280,7 +276,7 @@ PrewhereInfoPtr SerializedPlanParser::parsePreWhereInfo(const substrait::Express
     // for in function
     if (rel.has_singular_or_list())
     {
-        const auto *in_node = parseArgument(prewhere_info->prewhere_actions, rel);
+        const auto * in_node = parseArgument(prewhere_info->prewhere_actions, rel);
         prewhere_info->prewhere_actions->addOrReplaceInIndex(*in_node);
         filter_name = in_node->result_name;
     }
@@ -311,7 +307,7 @@ PrewhereInfoPtr SerializedPlanParser::parsePreWhereInfo(const substrait::Express
     {
         prewhere_info->prewhere_actions->removeUnusedActions(Names{filter_name}, false, true);
         prewhere_info->prewhere_actions->projectInput(false);
-        for (const auto& name : input.getNames())
+        for (const auto & name : input.getNames())
         {
             prewhere_info->prewhere_actions->tryRestoreColumn(name);
         }
@@ -674,12 +670,14 @@ QueryPlanPtr SerializedPlanParser::parseOp(const substrait::Rel & rel)
                     }
                 }
 
-                if (need_convert) {
+                if (need_convert)
+                {
                     ActionsDAGPtr convert_action
                         = ActionsDAG::makeConvertingActions(source, target, DB::ActionsDAG::MatchColumnsMode::Position);
                     if (convert_action)
                     {
-                        QueryPlanStepPtr convert_step = std::make_unique<ExpressionStep>(query_plan->getCurrentDataStream(), convert_action);
+                        QueryPlanStepPtr convert_step
+                            = std::make_unique<ExpressionStep>(query_plan->getCurrentDataStream(), convert_action);
                         convert_step->setStepDescription("Convert Aggregate Output");
                         query_plan->addStep(std::move(convert_step));
                     }
@@ -792,10 +790,8 @@ void SerializedPlanParser::addPreProjectStepIfNeeded(
             throw Exception(ErrorCodes::UNKNOWN_TYPE, "unsupported aggregate argument type {}.", arg.DebugString());
         }
 
-        if (which_measure_type.isNullable() &&
-            measure.measure().phase() == substrait::AGGREGATION_PHASE_INITIAL_TO_INTERMEDIATE &&
-            !expression->findInIndex(measure_name).result_type->isNullable()
-            )
+        if (which_measure_type.isNullable() && measure.measure().phase() == substrait::AGGREGATION_PHASE_INITIAL_TO_INTERMEDIATE
+            && !expression->findInIndex(measure_name).result_type->isNullable())
         {
             to_wrap_nullable.emplace_back(measure_name);
             need_pre_project = true;
@@ -803,7 +799,8 @@ void SerializedPlanParser::addPreProjectStepIfNeeded(
     }
     wrapNullable(to_wrap_nullable, expression, nullable_measure_names);
 
-    if (need_pre_project) {
+    if (need_pre_project)
+    {
         auto expression_before_aggregate = std::make_unique<ExpressionStep>(input, expression);
         expression_before_aggregate->setStepDescription("Before Aggregate");
         plan.addStep(std::move(expression_before_aggregate));
@@ -830,10 +827,13 @@ QueryPlanStepPtr SerializedPlanParser::parseAggregate(QueryPlan & plan, const su
 
     if (phase_set.size() > 1)
     {
-        if (phase_set.size() == 2 && has_first_stage && has_inter_stage) {
+        if (phase_set.size() == 2 && has_first_stage && has_inter_stage)
+        {
             // this will happen in a sql like:
             // select sum(a), count(distinct b) from T
-        } else {
+        }
+        else
+        {
             throw Exception(ErrorCodes::LOGICAL_ERROR, "too many aggregate phase!");
         }
     }
@@ -886,7 +886,8 @@ QueryPlanStepPtr SerializedPlanParser::parseAggregate(QueryPlan & plan, const su
         // if measure arg has nullable version, use it
         auto input_column = measure_names.at(i);
         auto entry = nullable_measure_names.find(input_column);
-        if (entry != nullable_measure_names.end()) {
+        if (entry != nullable_measure_names.end())
+        {
             input_column = entry->second;
         }
         agg.arguments = ColumnNumbers{plan.getCurrentDataStream().header.getPositionByName(input_column)};
@@ -1411,9 +1412,7 @@ QueryPlanPtr SerializedPlanParser::parse(const std::string & plan)
 QueryPlanPtr SerializedPlanParser::parseJson(const std::string & json_plan)
 {
     auto plan_ptr = std::make_unique<substrait::Plan>();
-    google::protobuf::util::JsonStringToMessage(
-        google::protobuf::stringpiece_internal::StringPiece(json_plan.c_str()),
-        plan_ptr.get());
+    google::protobuf::util::JsonStringToMessage(google::protobuf::stringpiece_internal::StringPiece(json_plan.c_str()), plan_ptr.get());
     return parse(std::move(plan_ptr));
 }
 
@@ -1631,8 +1630,8 @@ void SerializedPlanParser::removeNullable(std::vector<String> require_columns, A
     }
 }
 
-void SerializedPlanParser::wrapNullable(std::vector<String> columns, ActionsDAGPtr actionsDag,
-                                        std::map<std::string, std::string>& nullable_measure_names)
+void SerializedPlanParser::wrapNullable(
+    std::vector<String> columns, ActionsDAGPtr actionsDag, std::map<std::string, std::string> & nullable_measure_names)
 {
     for (const auto & item : columns)
     {
@@ -1784,8 +1783,33 @@ Block & LocalExecutor::getHeader()
 {
     return header;
 }
-LocalExecutor::LocalExecutor(QueryContext & _query_context)
-    : query_context(_query_context)
+std::map<String, size_t> LocalExecutor::getCurrentEventCounters()
+{
+    std::map<String, size_t> event_counters;
+    auto metrics = context->getConfigRef().getString("metrics_on_spark_ui", "");
+    static std::unordered_map<String, size_t> metric_index_map;
+    static std::once_flag init_flag;
+    std::call_once(
+        init_flag,
+        []() -> void
+        {
+            for (size_t i = 0; i < ProfileEvents::Counters::num_counters; i++)
+            {
+                metric_index_map.emplace(ProfileEvents::getName(i), i);
+            }
+        });
+    DB::CurrentThread::finalizePerformanceCounters();
+    Poco::StringTokenizer events(metrics, ",");
+    for (const auto & event : events)
+    {
+        auto & count = DB::CurrentThread::getProfileEvents()[metric_index_map[event]];
+        event_counters[event] = count;
+    }
+    return event_counters;
+}
+
+
+LocalExecutor::LocalExecutor(QueryContext & _query_context, ContextPtr context_) : query_context(_query_context), context(context_)
 {
 }
 }

loudongfeng/KyligenceClickHouse

utils/local-engine/Common/CHUtil.cpp
@@ -25,6 +25,9 @@
 #include <Poco/Logger.h>
 #include <Common/CHUtil.h>
 #include <Common/typeid_cast.h>
+#include <Core/ColumnsWithTypeAndName.h>
+#include <fmt/core.h>
+#include <DataTypes/DataTypesNumber.h>
 
 namespace DB
 {
@@ -257,6 +260,21 @@ DB::NamesAndTypesList MergeTreeUtil::getSchemaFromMergeTreePart(const fs::path &
     return names_types_list;
 }
 
+void ProfileEventsUtil::logProfileEvents(std::map<String, size_t> events)
+{
+    auto * logger = &Poco::Logger::get("ProfileEventsUtil");
+    if (events.empty() || !logger->debug())
+    {
+        return;
+    }
+
+    std::vector<String> event_strings;
+    for (auto& [name, count] : events)
+    {
+        event_strings.emplace_back(fmt::format("\t{} : {}", name, count));
+    }
+    LOG_DEBUG(logger, "Current Profile Counters:\n{}\n", fmt::join(event_strings, "\n"));
+}
 
 NestedColumnExtractHelper::NestedColumnExtractHelper(const DB::Block & block_, bool case_insentive_)
     : block(block_)

utils/local-engine/Parser/SerializedPlanParser.cpp
@@ -11,16 +11,15 @@
 #include <Core/Names.h>
 #include <Core/NamesAndTypes.h>
 #include <DataTypes/DataTypeAggregateFunction.h>
+#include <DataTypes/DataTypeArray.h>
 #include <DataTypes/DataTypeDate32.h>
 #include <DataTypes/DataTypeDateTime64.h>
+#include <DataTypes/DataTypeMap.h>
 #include <DataTypes/DataTypeSet.h>
 #include <DataTypes/DataTypeString.h>
+#include <DataTypes/DataTypeTuple.h>
 #include <DataTypes/DataTypesDecimal.h>
 #include <DataTypes/DataTypesNumber.h>
-#include <DataTypes/DataTypeDateTime64.h>
-#include <DataTypes/DataTypeArray.h>
-#include <DataTypes/DataTypeMap.h>
-#include <DataTypes/DataTypeTuple.h>
 #include <Functions/FunctionFactory.h>
 #include <Functions/registerFunctions.h>
 #include <Interpreters/ActionsDAG.h>
@@ -48,11 +47,13 @@
 #include <google/protobuf/wrappers.pb.h>
 #include <Poco/StringTokenizer.h>
 #include <Poco/Util/MapConfiguration.h>
-#include <Common/DebugUtils.h>
 #include <Common/JoinHelper.h>
 #include <Common/MergeTreeTool.h>
 #include <Common/StringUtils.h>
 #include <QueryPipeline/QueryPipelineBuilder.h>
+#include <Interpreters/QueryPriorities.h>
+#include <Interpreters/ProcessList.h>
+
 
 #include <Processors/QueryPlan/QueryPlan.h>
 #include <Processors/QueryPlan/SortingStep.h>
@@ -311,9 +312,9 @@ QueryPlanPtr SerializedPlanParser::parseReadRealWithJavaIter(const substrait::Re
 
 void SerializedPlanParser::addRemoveNullableStep(QueryPlan & plan, std::vector<String> columns)
 {
-    if (columns.empty()) return;
-    auto remove_nullable_actions_dag
-        = std::make_shared<ActionsDAG>(blockToNameAndTypeList(plan.getCurrentDataStream().header));
+    if (columns.empty())
+        return;
+    auto remove_nullable_actions_dag = std::make_shared<ActionsDAG>(blockToNameAndTypeList(plan.getCurrentDataStream().header));
     removeNullable(columns, remove_nullable_actions_dag);
     auto expression_step = std::make_unique<ExpressionStep>(plan.getCurrentDataStream(), remove_nullable_actions_dag);
     expression_step->setStepDescription("Remove nullable properties");
@@ -334,7 +335,7 @@ QueryPlanPtr SerializedPlanParser::parseMergeTreeTable(const substrait::ReadRel 
     else
     {
         // For count(*) case, there will be an empty base_schema, so we try to read at least once column
-        auto all_parts_dir = MergeTreeUtil::getAllMergeTreeParts( std::filesystem::path("/") / merge_tree_table.relative_path);
+        auto all_parts_dir = MergeTreeUtil::getAllMergeTreeParts(std::filesystem::path("/") / merge_tree_table.relative_path);
         if (all_parts_dir.empty())
         {
             throw DB::Exception(DB::ErrorCodes::LOGICAL_ERROR, "Empty mergetree directory: {}", merge_tree_table.relative_path);
@@ -393,13 +394,14 @@ QueryPlanPtr SerializedPlanParser::parseMergeTreeTable(const substrait::ReadRel 
     if (!not_null_columns.empty())
     {
         auto input_header = query->getCurrentDataStream().header;
-        std::erase_if(not_null_columns, [input_header](auto item) -> bool {return !input_header.has(item);});
+        std::erase_if(not_null_columns, [input_header](auto item) -> bool { return !input_header.has(item); });
         addRemoveNullableStep(*query, not_null_columns);
     }
     return query;
 }
 
-PrewhereInfoPtr SerializedPlanParser::parsePreWhereInfo(const substrait::Expression & rel, Block & input, std::vector<String>& not_nullable_columns)
+PrewhereInfoPtr
+SerializedPlanParser::parsePreWhereInfo(const substrait::Expression & rel, Block & input, std::vector<String> & not_nullable_columns)
 {
     auto prewhere_info = std::make_shared<PrewhereInfo>();
     prewhere_info->prewhere_actions = std::make_shared<ActionsDAG>(input.getNamesAndTypesList());
@@ -407,7 +409,7 @@ PrewhereInfoPtr SerializedPlanParser::parsePreWhereInfo(const substrait::Express
     // for in function
     if (rel.has_singular_or_list())
     {
-        const auto *in_node = parseArgument(prewhere_info->prewhere_actions, rel);
+        const auto * in_node = parseArgument(prewhere_info->prewhere_actions, rel);
         prewhere_info->prewhere_actions->addOrReplaceInOutputs(*in_node);
         filter_name = in_node->result_name;
     }
@@ -437,7 +439,7 @@ PrewhereInfoPtr SerializedPlanParser::parsePreWhereInfo(const substrait::Express
     {
         prewhere_info->prewhere_actions->removeUnusedActions(Names{filter_name}, false, true);
         prewhere_info->prewhere_actions->projectInput(false);
-        for (const auto& name : input.getNames())
+        for (const auto & name : input.getNames())
         {
             prewhere_info->prewhere_actions->tryRestoreColumn(name);
         }
@@ -801,7 +803,8 @@ QueryPlanPtr SerializedPlanParser::parseOp(const substrait::Rel & rel, std::list
                         = ActionsDAG::makeConvertingActions(source, target, DB::ActionsDAG::MatchColumnsMode::Position);
                     if (convert_action)
                     {
-                        QueryPlanStepPtr convert_step = std::make_unique<ExpressionStep>(query_plan->getCurrentDataStream(), convert_action);
+                        QueryPlanStepPtr convert_step
+                            = std::make_unique<ExpressionStep>(query_plan->getCurrentDataStream(), convert_action);
                         convert_step->setStepDescription("Convert Aggregate Output");
                         query_plan->addStep(std::move(convert_step));
                     }
@@ -929,10 +932,8 @@ void SerializedPlanParser::addPreProjectStepIfNeeded(
             throw Exception(ErrorCodes::UNKNOWN_TYPE, "unsupported aggregate argument type {}.", arg.DebugString());
         }
 
-        if (which_measure_type.isNullable() &&
-            measure.measure().phase() == substrait::AGGREGATION_PHASE_INITIAL_TO_INTERMEDIATE &&
-            !expression->findInOutputs(measure_name).result_type->isNullable()
-            )
+        if (which_measure_type.isNullable() && measure.measure().phase() == substrait::AGGREGATION_PHASE_INITIAL_TO_INTERMEDIATE
+            && !expression->findInOutputs(measure_name).result_type->isNullable())
         {
             to_wrap_nullable.emplace_back(measure_name);
             need_pre_project = true;
@@ -1631,9 +1632,7 @@ QueryPlanPtr SerializedPlanParser::parse(const std::string & plan)
 QueryPlanPtr SerializedPlanParser::parseJson(const std::string & json_plan)
 {
     auto plan_ptr = std::make_unique<substrait::Plan>();
-    google::protobuf::util::JsonStringToMessage(
-        google::protobuf::stringpiece_internal::StringPiece(json_plan.c_str()),
-        plan_ptr.get());
+    google::protobuf::util::JsonStringToMessage(google::protobuf::stringpiece_internal::StringPiece(json_plan.c_str()), plan_ptr.get());
     return parse(std::move(plan_ptr));
 }
 
@@ -1853,8 +1852,8 @@ void SerializedPlanParser::removeNullable(std::vector<String> require_columns, A
     }
 }
 
-void SerializedPlanParser::wrapNullable(std::vector<String> columns, ActionsDAGPtr actionsDag,
-                                        std::map<std::string, std::string>& nullable_measure_names)
+void SerializedPlanParser::wrapNullable(
+    std::vector<String> columns, ActionsDAGPtr actionsDag, std::map<std::string, std::string> & nullable_measure_names)
 {
     for (const auto & item : columns)
     {
@@ -1884,11 +1883,19 @@ void LocalExecutor::execute(QueryPlanPtr query_plan)
     Stopwatch stopwatch;
     stopwatch.start();
     QueryPlanOptimizationSettings optimization_settings{.optimize_plan = true};
+    DB::QueryPriorities priorities;
+    auto query_status = std::make_shared<DB::QueryStatus>(context,
+                                                          "query",
+                                                          context->getClientInfo(),
+                                                          priorities.insert(static_cast<int>(context->getSettingsRef().priority)),
+                                                          std::move(DB::CurrentThread::getGroup()),
+                                                          DB::IAST::QueryKind::Select, 0);
     auto pipeline_builder = current_query_plan->buildQueryPipeline(
         optimization_settings,
         BuildQueryPipelineSettings{
             .actions_settings = ExpressionActionsSettings{
-                .can_compile_expressions = true, .min_count_to_compile_expression = 3, .compile_expressions = CompileExpressions::yes}});
+                .can_compile_expressions = true, .min_count_to_compile_expression = 3, .compile_expressions = CompileExpressions::yes},
+        .process_list_element = query_status});
     query_pipeline = QueryPipelineBuilder::getPipeline(std::move(*pipeline_builder));
     auto t_pipeline = stopwatch.elapsedMicroseconds();
     executor = std::make_unique<PullingPipelineExecutor>(query_pipeline);
@@ -1970,8 +1977,38 @@ Block & LocalExecutor::getHeader()
 {
     return header;
 }
-LocalExecutor::LocalExecutor(QueryContext & _query_context)
-    : query_context(_query_context)
+std::map<String, size_t> LocalExecutor::getCurrentEventCounters()
+{
+    std::map<String, size_t> event_counters;
+    auto metrics = context->getConfigRef().getString("metrics_on_spark_ui", "");
+    static std::unordered_map<String, size_t> metric_index_map;
+    static std::once_flag init_flag;
+    std::call_once(
+        init_flag,
+        []() -> void
+        {
+            for (size_t i = 0; i < ProfileEvents::Counters::num_counters; i++)
+            {
+                metric_index_map.emplace(ProfileEvents::getName(i), i);
+            }
+        });
+    DB::CurrentThread::finalizePerformanceCounters();
+    Poco::StringTokenizer events(metrics, ",");
+    for (const auto & event : events)
+    {
+        auto & count = DB::CurrentThread::getProfileEvents()[metric_index_map[event]];
+        event_counters[event] = count;
+    }
+    for (const auto & item : query_pipeline.getProcessors())
+    {
+//        event_counters[item->getName() + "_input_wait"] = item->getInputWaitElapsedUs();
+        event_counters[item->getName() + "_elapsed"] = item->getElapsedUs();
+    }
+    return event_counters;
+}
+
+
+LocalExecutor::LocalExecutor(QueryContext & _query_context, ContextPtr context_) : query_context(_query_context), context(context_)
 {
 }
 }