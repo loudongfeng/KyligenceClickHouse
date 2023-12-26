cb0e64b84f80ad65a12c309479d0aeb780a85f7b
lgbo <lgbo.ustc@gmail.com>
2023-12-26 17:58:58 +0800

    Add GlutenAggregatorUtil to expose APIs into gluten (#476)
    (cherry picked from commit 890b6d7bb527560f3e697c6cf9d3856190cb59c7)
    (cherry picked from commit 0abe87e2cf31260f63e9f8c281bc321bf47f865d)
    (cherry picked from commit 5a0a20ee12c33b40425184f9cfca32d986783be8)
    (cherry picked from commit 632a6831f2e270cf741b527085d3824b8d402a43)
    (cherry picked from commit 234ed8632dfc21588fdb2dd86d47a6c8d6d49f69)

--- src/Interpreters/Aggregator.h
+++ src/Interpreters/Aggregator.h
@@ -1264,6 +1264,7 @@ private:
     friend class ConvertingAggregatedToChunksSource;
     friend class ConvertingAggregatedToChunksWithMergingSource;
     friend class AggregatingInOrderTransform;
+    friend class GlutenAggregatorUtil;
 
     /// Data structure of source blocks.
     Block header;
